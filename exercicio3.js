1d9af77ecde3f965aa9eec22fa544825e14ab4b2
Gisele <gisele.nnubia@gmail.com>
2026-04-03 00:24:28 -0300

    ex 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,10 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let itensDaListaOrdenada = document.querySelectorAll('#aliens p');
+
+itensDaListaOrdenada.forEach(function(itemEl) {
+  itemEl.addEventListener('click', function() {
+    itemEl.classList.toggle('expandido')
+  })
+});
