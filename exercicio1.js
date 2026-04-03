0f44c4dce448d6f0780f2a4919a782482c004b29
Gisele <gisele.nnubia@gmail.com>
2026-04-02 23:28:04 -0300

    ex1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let calcularEl = document.querySelector('#calcular');
+let resultadoEl = document.querySelector('#resultado');
+
+f_calc = function() {
+    let m1 = document.querySelector('#massa1').value;
+    let m2 = document.querySelector('#massa2').value;
+    let g = document.querySelector('#constante').value;
+    let d = document.querySelector('#distancia').value;
+
+    f_grav = (g*m1 * m2) / d**2;
+    resultadoEl.value = f_grav;
+}
+
+calcularEl.addEventListener('click', f_calc);
